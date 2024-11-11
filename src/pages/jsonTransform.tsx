0e1cb35b6c057f019d705eb425dd6b5d6df942c2
NicoleNA1101 <82178025+NicoleNA1101@users.noreply.github.com>
2024-11-11 11:40:40 -0300

    Cambios 11/11

--- src/pages/jsonTransform.tsx
+++ src/pages/jsonTransform.tsx
@@ -1,134 +1,204 @@
-import { Textarea, Box, Button } from '@chakra-ui/react';
-import { useState } from 'react';
-
-export default function newExercise() {
-  const [textInput, setTextInput] = useState('');
-  const [jsonOutput, setJsonOutput] = useState('');
-
-  const handleTransformToJson = () => {
-    // JSON base
-    let jsonResult = {
-      code: "",
-      meta: {},
-      text: "",
-      type: "lvltutor",
-      steps: [
+/*
+json guia con anotaciones mias
+{
+  "eqc": "",   no es necesario, expresion del enunciado
+  "img": "",	no es necesario, imagen del paso
+  "code": "succ1", necesario codigo del ejercicio	
+  "meta": {}, no se lo que es, hay que dejarlo para que no de error 
+  "text": "", no es necesario, texto enunciado
+  "type": "lvltutor2", 100% necesario
+  "steps": [ arreglo de pasos del ejercicio, necesario
+      {
+      "StepType": "MultiplePlaceholders", tipo de paso, necesario
+      "stepTitle": "Paso 1: Represente el término de la sucesión", titulo del paso, 
+      "stepId": "0",
+      "displayResult": [
+        "B_1 = \\frac{{(-1)^\\placeholder[a]{n} \\cdot (\\placeholder[b]{n}^2 - \\placeholder[c]{n})}}{{\\placeholder[d]{n}^2 + 1}}"
+      ], cumple como expresion en los ejercicios de tipo single placeholder, multiple placeholder, alternativas
+      "expression": "", expresion del paso se usa en verdadero y falso, en los de tipo tabla 
+      "KCs": ["","",""], arreglo de kcs del paso, necesaario
+      "hints": [ arreglo de hints del paso, necesario
         {
-          KCs: ["IdeTer2"],
-          hints: [
-            {
-              hint: "La fórmula de cuadrado de binomio es a²+2ab+b²=(a+b)², utiliza el término del medio para obtener el segundo término",
-              hintId: 0
-            },
-            {
-              hint: "Teniendo el término del medio dividido en 2, debes encontrar el número el cual multiplicado por el primer número del cuadrado de binomio (1x) te da como resultado dicho término (1x*?=4). Finalmente, eleva el número resultante al cuadrado",
-              hintId: 1
-            },
-            { hint: "El resultado es 16.", hintId: 2 }
-          ],
-          stepId: "0",
-          values: [{ name: "x", value: 1 }],
-          answers: [{ answer: ["16"], nextStep: "2" }],
-          summary: "1) Se obtiene término faltante.",
-          stepTitle: "Encuentra el término faltante '?'",
-          correctMsg: "Has encontrado el término.",
-          expression: "x²+8x+'?'",
-          validation: "evaluateAndCount",
-          incorrectMsg: "Revisa que la expresión ingresada este correcta.",
-          displayResult: ["16"],
-          matchingError: []
+          "hint": "", texto de la hint, necesario
+          "hintId": 0 id de la hint, necesario y autogenerado
         },
         {
-          KCs: ["IdeTer2"],
-          hints: [
-            {
-              hint: "La fórmula de cuadrado de binomio es a²+2ab+b²=(a+b)², y en el paso anterior encontraste el término 'b²'.",
-              hintId: 0
-            },
-            {
-              hint: "Para encontrar el segundo término del cuadrado de binomio, debes calcular la raíz cuadrada del término obtenido en el paso anterior",
-              hintId: 1
-            },
-            { hint: "El resultado es (x+4)².", hintId: 2 }
-          ],
-          stepId: "2",
-          values: [{ name: "x", value: 2 }],
-          answers: [{ answer: ["(x+4)^{2}"], nextStep: "-1" }],
-          summary: "2) Se obtiene el cuadrado de binomio.",
-          stepTitle: "Representa la expresión como un cuadrado de binomio.",
-          correctMsg: "Has encontrado el cuadrado de binomio.",
-          expression: "x²+8x+16",
-          validation: "evaluateAndCount",
-          incorrectMsg: "Revisa que la expresión ingresada este correcta.",
-          displayResult: ["(x+4)^{2}"],
-          matchingError: []
+          "hint": "",
+          "hintId": 1
+        },
+        {
+          "hint": "",
+          "hintId": 2
         }
       ],
-      title: "Cuadrado de binomio"
-    };
-
-    // Función para actualizar campos específicos en el JSON
-    const updateJsonWithInput = (input) => {
-      const lines = input.split('\n'); // Dividir el texto en líneas por saltos de línea
-      
-      lines.forEach((line) => {
-        const [key, ...valueArr] = line.split(':'); 
-        const value = valueArr.join(':').trim();
-        switch (key.trim().toLowerCase()) {
-          case 'code':
-            jsonResult.code = value;
-            break;
-          case 'type':
-            jsonResult.type = value;
-            break;
-          case 'meta':
-            try {
-              jsonResult.meta = JSON.parse(value);
-            } catch (error) {
-              console.error("Error parsing meta:", error);
-            }
-            break;
-          case 'text':
-            jsonResult.text = value;
-            break;
-          default:
-            break;
+      "values": [], valores en caso de alternativas
+      "answers": [ arreglo tiene respuesta y paso siguente
+        {
+          "answer": [ arreglo de respuestas varia segun el tipo de paso el como se escribe
+            "1",
+            "1",
+            "1",
+            "1"
+          ],
+          "nextStep": "1" necesario id del siguente paso valor = -1 si es que es el ultimo paso
         }
-      });
-    };
+      ],
+      "summary": "1)Luego se comprueban valores del conjunto", resumen del paso 
+      "correctMsg": "La representacion esta correcta",	mensaje de exito de resolucion del paso
+      "incorrectMsg": "Compruebe su respuesta", mensjae de fallo de resolucion del paso
+      "matchingError": [] no he encontrado donde se ocupa
+    },
+    {
+      "KCs": [],
+      "hints": [
+        {
+          "hint": "Debe representar la respuesta como fraccion",
+          "hintId": 0
+        },
+        {
+          "hint": "La respuesta es: ",
+          "hintId": 1,
+          "expression": "0"
+        }
+      ],
+      "stepId": "1",
+      "values": [],
+      "answers": [
+        {
+          "answer": [
+            "0"
+          ],
+          "nextStep": "2"
+        },
+        {
+          "answer": [
+            "-\\frac{1}{3}"
+          ],
+          "nextStep": "2"
+        },
+        {
+          "answer": [
+            "\\frac{-1}{3}"
+          ],
+          "nextStep": "2"
+        }
+      ],
+      "summary": "1)Luego se comprueban valores del conjunto",
+      "StepType": "SinglePlaceholder",
+      "stepTitle": "Paso 2: Resuelva el primer valor de la sucesión, si no es entero, represéntelo como fracción de la forma $\\frac{a}{b}$",
+      "correctMsg": "El valor de B1 es correcto",
+      "expression": "",
+      "incorrectMsg": "Compruebe su respuesta",
+      "displayResult": [
+        "\\placeholder[a]{}"
+      ],
+      "matchingError": []
+    },
+    {
+      "KCs": [
+        "CalcValSuc"
+      ],
+      "hints": [
+        {
+          "hint": "Debes calcular B3 y B4",
+          "hintId": 0
+        },
+        {
+          "hint": "Expresa los valores como fraccion",
+          "hintId": 1
+        },
+        {
+          "hint": "Los valores son: ",
+          "hintId": 0,
+          "expression": "-\\frac{3}{5} y \\frac{12}{17}"
+        }
+      ],
+      "stepId": "2",
+      "values": [],
+      "answers": [
+        {
+          "answer": [
+            "-\\frac{3}{5} ",
+            "\\frac{12}{17}"
+          ],
+          "nextStep": "-1"
+        }
+      ],
+      "summary": "1)Luego se comprueban valores del conjunto",
+      "StepType": "MultiplePlaceholders",
+      "stepTitle": "Paso 3: Encuentre los primeros 4 términos de la sucesión",
+      "correctMsg": "La sucesion corresponde",
+      "expression": "",
+      "validation": "evaluate",
+      "incorrectMsg": "Compruebe su respuesta",
+      "displayResult": [
+        "0,\\; \\frac{2}{5}  ,\\;   \\placeholder[a]{},\\; \\placeholder[b]{}"
+      ],
+      "matchingError": []
+    }
+  ],
 
-    updateJsonWithInput(textInput);
+}
+*/
 
-    setJsonOutput(JSON.stringify(jsonResult, null, 2));
+const stepTypeMapping: { [key: string]: string } = {
+  "alternativas": "Alternatives",
+  "verdadero/falso": "TrueFalse",
+  "singleplaceholder": "SinglePlaceholder",
+  "multipleplaceholder": "MultiplePlaceholder",
+  "table": "TableStep"
+};
+
+export function generarEjercicioJSON(cards: any[], code:string) {
+  const ejercicio = {
+    title:""||"Titulo por defecto", //titulo del ejercicio distinot al enunciado
+    eqc: ""||"Expresion por defecto enunciado",   // expresión opcional, utilizado en algunos ejercicios y en otros no (ejemplo en el de succ1, en otros como uni1 ni siquiera existe el parametro)
+    img: "",   // imagen opcional no lo tratamos nostros
+    code: code, // código del ejercicio se extrae directamente del modal que se abre al guardar el ejercico (parametro de la funcion)
+    meta: {}, // campo necesario, aunque vacío
+    text: cards[0].title||"Enunciado por defecto", // texto opcional, texto del enunciado hay ejercicio que lo utilizan otros no, se extraer directamente de la tarjeta enunciado
+    type: "lvltutor2", // tipo de implementacion a invocar, estático y obligatorio
+    steps: cards.slice(1).map((card, index) => ({ //arreglo de steps con todo lo que tienen que llevar, el slice corta la primera tarjeta que es la del enunciado
+      StepType: stepTypeMapping[card.type] || "", // tipo de paso utiliza el map que cree arriba para no tener que cambiar nada en newExercise
+      stepTitle: card.title || "Titulo paso por defecto", // título del paso
+      stepId: index.toString(), // ID progresivo del paso autogenerado
+      displayResult: [card.expression] || ["Expresion por defecto"], // debe ser una lista no entiendo por que, cumple como expresion en los ejercicios de tipo single placeholder, multiple placeholder, alternativas
+      expression: card.expression || "Expresion por defecto", // expresión del paso se usa en verdadero y falso, en los de tipo tabla de verdad
+      KCs: [card.kcs] || ["kc_por_defecto_no_se_asigno_Kc"], // arreglo de KCs
+      hints: (card.hints || []).map((hint: any, hintIndex: number) => ({ // arreglo de hints
+        hint: hint.text || "Texto hint por defecto", // texto de la hint
+        hintId: hintIndex // ID progresivo de la hint
+      })),
+      values: stepTypeMapping[card.type] === "Alternatives" ? card.alternatives.map((alternative: any) => ({ 
+        name: alternative.text || [], // nombre de la alternativa, aun que en la implementacion no se utiliza lo dejo aqui asi para futuro
+        value: alternative.text || [] // valor de la alternativa, es el contenido este es el que se utiliza para verificar cual es la correcta
+      }))//true or false no lo ocupa sin embargo hay ejercicios que si lo tienen, multiplaceholder tampoco, single placeholder tampoco y table tampoco
+      || []: card.values,
+      /*
+      Uso exlcusivo de los ejercicios de tipo tablas, cacho maximo
+
+      table: {"rows":[{"data":["","",""]},{},{}], "header":[{"align": "center","value": ""}],"alignRows": "center","tableCaption": ""},
+      button: [["V","F"],["V","F"],["V","F"],["V","F"]], */
+      
+      //answers tiene el arreglo de respuestas que varia segun el tipo de ejercicio y next step que es el paso siguente y sera -1 si es el ultimo
+      answers: stepTypeMapping[card.type] === "Alternatives" ?[{"answer":[(card.alternatives?.find((alt: any) => alt.correct)?.text || "")]/*Busca el primer elemento que sea correcto*/, "nextStep": index==cards.length - 2 ? "-1": index+1 /* -1 si es el último paso */}]:"", 
+      summary: card.summary || "Resumen por defecto", // resumen del paso
+      correctMsg: card.correctMsg || "Mensaje correcto por defecto", // mensaje de éxito
+      incorrectMsg: card.incorrectMsg || "Mensaje fallo por defecto", // mensaje de error
+      matchingError: card.matchingError || [] // no he encontrado donde se ocupa no entie do para que es
+    }))
   };
 
-  return (
-    <>
-      <div>
-        <Box display="flex" flexDirection="row" gap={4}>
-          <Box>
-            <Textarea
-              width="400px"
-              height="200px"
-              placeholder="Inserte texto aquí (code: valor, type: valor, meta: {...})"
-              value={textInput}
-              onChange={(e) => setTextInput(e.target.value)}
-            />
-            <Button mt={2} onClick={handleTransformToJson}>
-              Transformar a JSON
-            </Button>
-          </Box>
-          <Box>
-            <Textarea
-              width="400px"
-              height="200px"
-              placeholder="Resultado JSON"
-              value={jsonOutput}
-              readOnly
-            />
-          </Box>
-        </Box>
-      </div>
-    </>
-  );
+  return JSON.stringify(ejercicio, null, 2); // Formateado para facilidad de lectura
+}
+
+export function descargarJSON(ejercicioJSON: string, nombreArchivo = "ejercicio.json") {
+  const blob = new Blob([ejercicioJSON], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = nombreArchivo;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 }

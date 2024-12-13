b431c31d41ac7816a921942687e2e5e078f687fa
Ravioks <martinsuarez5.1@gmail.com>
2024-12-13 12:58:30 -0300

    Limpieza de archivos

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -31,37 +31,10 @@ export function Navigation() {
               Crear ejercicio
             </SidebarLink>
 
-            <SidebarLink icon={<FaPlusCircle />} href={"SebaAlternativas"}>
-              Alternativas
-            </SidebarLink>
-
-            <SidebarLink icon={<FaPlusCircle />} href={"multiplaceholders"}>
-              Multiplaceholder
-            </SidebarLink>
-
-            <SidebarLink icon={<FaWrench />} href={"selectByCode"}>
-              Modificar ejercicio
-            </SidebarLink>
-
-            <SidebarLink icon={<FaWrench />} href={"jsonTransform"}>
-            transformador Json 
-            </SidebarLink>
-
-            <SidebarLink icon={<FaWrench />} href={"previsualizar"}>
-            Previsualizar 
-            </SidebarLink>
-
-            <SidebarLink icon={<FaPlusCircle />} href={"SebaModalGuardado"}>
-            Testing
-            </SidebarLink>
-
             <SidebarLink icon={<FaWrench />} href={"verJSON"}>
             Ver JSON
             </SidebarLink>
 
-            <SidebarLink icon={<FaPlusCircle />} href={"SebaVyF"}>
-            Verdadero y Falso
-            </SidebarLink>
           </>
         )}
       </Stack>
